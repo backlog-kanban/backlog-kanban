595e09644d9ae73d50a6cc4f5f175285a7c0aa3e
chokkoyamada <yamada_naoyuki@cyberagent.co.jp>
2013-11-17 11:44:07 +0900

    railsプロジェクトの作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BacklogKanban::Application.config.secret_key_base = '5bb13bc1c2bf64d47e29325ecfbdae1e2e1aa54c7f6181d4a0655840d0a4dbeb48071f8301d3eda8d584cd0317d72edec9d4ed569a77bba020c13bcca7ba9512'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BacklogKanban::Application.config.session_store :cookie_store, key: '_backlog-kanban_session'
