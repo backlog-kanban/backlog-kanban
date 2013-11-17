32bb423004c2e35d74932d9fbec3973ab6687e38
chokkoyamada <yamada_naoyuki@cyberagent.co.jp>
2013-11-17 19:55:41 +0900

    configの管理方法を変更

---
+++ config/initializers/backlog_config.rb
@@ -0,0 +1,4 @@
+BacklogKanban::Application.config.host = 'kanban.backlog.jp'
+BacklogKanban::Application.config.project_id = 1073817496
+BacklogKanban::Application.config.user = 'api'
+BacklogKanban::Application.config.password = 'api1117'
