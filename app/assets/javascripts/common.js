733a08252a763a4fd62f2ac1f756b9992d9bfddf
oogushi.takuya <oogushi.takuya@members.co.jp>
2013-11-17 15:23:23 +0900

    形式テスト

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,7 +1,15 @@
-jQuery(function(){
-	var data = {
-					condition: ["todo","do","done","finish"]
-				};
-	console.log(data["condition"]);
+/*jQuery(function(){
+	var state = ["todo","do","done","finish"]
+	jQuery.ajax({
+		url: "/issues.json",
+		dataType: "json",
+		data: {
+		},
+		success: function(data){
+	console.log(data);
 	jQuery("#task").tmpl({data: data}).appendTo("#task_area");
-});
+	},
+		error: function(data){
+		}
+	});
+});*/
