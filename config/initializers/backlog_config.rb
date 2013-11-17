83551482dc7224eb28f5b9b0a666fdde641a04b4
chokkoyamada <yamada_naoyuki@cyberagent.co.jp>
2013-11-17 19:57:21 +0900

    公開するconfigファイルを削除

--- config/initializers/backlog_config.rb
+++
@@ -1,4 +0,0 @@
-BacklogKanban::Application.config.host = 'kanban.backlog.jp'
-BacklogKanban::Application.config.project_id = 1073817496
-BacklogKanban::Application.config.user = 'api'
-BacklogKanban::Application.config.password = 'api1117'
